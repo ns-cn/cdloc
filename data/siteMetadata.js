63e35735f12ed4ab94167d1ca7eae3007ff849f7
tangyujun <ns-cn@qq.com>
2026-07-16 14:47:51 +0800

    重构博客结构：添加日期前缀文件名、家电图片重命名、配置Giscus评论、评论自动加载

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -59,27 +59,17 @@ const siteMetadata = {
     // https://vercel.com/docs/environment-variables
     provider: 'giscus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
+      repo: 'ns-cn/cdloc',
+      repositoryId: 'R_kgDOTVZcmw',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOTVZcm84DBTTj',
+      mapping: 'pathname',
+      reactionsEnabled: true,
+      emitMetadata: false,
+      inputPosition: 'top',
       theme: 'light',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: 'zh-CN',
     },
   },
   search: {
